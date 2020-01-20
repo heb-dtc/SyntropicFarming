c4a49b7e13e628521cb60b68d1e61b4837b6e796
Florent NOEL <florent.noel@protonmail.com>
2020-01-20 19:03:19 +0100

    Box component
    The box component fetch a list of items and show/hide them on click.

---
+++ client/src/components/Box.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+
+function Box(props) {
+    const [showItems, setShowItems] = useState(false);
+    const [items, setItems] = useState([]);
+    const baseUrl = 'http://localhost:3001/api';
+
+        useEffect(() => {
+            const fetchItems = async () => {
+              const response = await axios(`${baseUrl}/${props.itemsEndpoint}`);
+              setItems(response.data);
+            };
+            fetchItems();
+          }, []);
+
+    return (
+        <div>
+            <div className="outline bg-white pv4 center" onClick={() => setShowItems(!showItems)}>
+            {
+                props.title
+            }
+            </div>
+            { showItems &&
+                <div>
+                    {
+                        items.map(item => {
+                            return <div>{item.species}</div>
+                        })
+                    }
+                </div>
+            }
+        </div>
+    );
+}
+
+export default Box
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,23 +1,28 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import Box from './Box'
 
 function Main() {
-    const [speciesItems, setSpeciesItems] = useState([]);
-
-    useEffect(() => {
-        const fetchSpecies = async () => {
-          const response = await axios(`http://localhost:3001/api/species`);
-          setSpeciesItems(response.data);
-        };
-        fetchSpecies();
-      }, []);
-
       return (
-          <div>
-            {speciesItems && speciesItems.length
-              ? speciesItems.length
-              : 'No species found'
-            }
+          <div className="mw9 center ph3-ns">
+
+            <div className="cf ph2-ns">
+              <div className="fl w-third-ns pa2">
+                <Box title={'species'} itemsEndpoint={'species'}/>
+                <Box title={'processes'} itemsEndpoint={'processes'}/>
+                <Box title={'materials'} itemsEndpoint={'materials'}/>
+                <Box title={'locations'} itemsEndpoint={'locations'}/>
+              </div>
+
+              <div className="fl w-100 w-third-ns pa2">
+                <div className="outline bg-white pv4"></div>
+              </div>
+
+              <div className="fl w-100 w-third-ns pa2">
+                <div className="outline bg-white pv4"></div>
+              </div>
+
+            </div>
           </div>
         );
 }
