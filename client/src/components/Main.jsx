e05154fdfd59824ec8beb5529d2931fcb2522d7c
Florent NOEL <florent.noel@protonmail.com>
2020-01-23 14:00:22 +0100

    Store search param globally in Context
    The current search params are stored in the global SearchContext object
    so that other top level React components can be aware of them.

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,29 +4,58 @@ import ProcessBox from './ProcessBox'
 import MaterialBox from './MaterialBox'
 import ClimateBox from './ClimateBox'
 
+export const SearchContext = React.createContext();
+const initialState = {
+    item: null,
+};
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "SEARCH_SPECIES":
+            return {
+                ...state,
+                item: action.payload
+            };
+        case "CLEAR_SEARCH":
+            return {
+                ...state,
+                item: null
+            };
+        default:
+            return state;
+    }
+};
+
 function Main() {
+    const [state, dispatch] = React.useReducer(reducer, initialState);
+
     return (
-        <div className="mw9 center ph3-ns">
+        <SearchContext.Provider value={{
+            state,
+            dispatch
+        }}>
+            <div className="mw9 center ph3-ns">
 
-            <div className="cf ph2-ns">
+                <div className="cf ph2-ns">
 
-                <div className="fl w-20 pa2 tc">
-                    <SpeciesBox/>
-                    <ProcessBox/>
-                    <MaterialBox/>
-                    <ClimateBox/>
+                    <div className="fl w-20 pa2 tc">
+                        <SpeciesBox/>
+                        <ProcessBox/>
+                        <MaterialBox/>
+                        <ClimateBox/>
+                    </div>
+
+                    <div className="fl w-60 pa2">
+                        <div className="outline bg-white pv4"></div>
+                    </div>
+
+                    <div className="fl w-20 pa2">
+                        <div className="outline bg-white pv4"></div>
+                    </div>
+
                 </div>
-
-                <div className="fl w-60 pa2">
-                    <div className="outline bg-white pv4"></div>
-                </div>
-
-                <div className="fl w-20 pa2">
-                    <div className="outline bg-white pv4"></div>
-                </div>
-
             </div>
-        </div>
+        </SearchContext.Provider>
     );
 }
 
--- client/src/components/SpeciesBox.jsx
+++ client/src/components/SpeciesBox.jsx
@@ -1,15 +1,22 @@
 import React, {useState} from "react";
 import Box from './Box'
+import {SearchContext} from '@/components/Main'
 
 const SpeciesBox = () => {
     const [items, setItems] = useState([]);
+    const {dispatch} = React.useContext(SearchContext);
 
     return (
         <Box title={'species'} itemsEndpoint={'species'} setItems={(items) => setItems(items)}>
             <div>
                 {
                     items.map(item => {
-                        return <div>{item.species}</div>
+                        return <div onClick={() => dispatch({
+                            type: "SEARCH_SPECIES",
+                            payload: item
+                        })}>
+                            {item.species}
+                        </div>
                     })
                 }
             </div>
