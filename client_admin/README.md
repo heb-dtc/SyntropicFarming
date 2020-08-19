c153dca61344de5cfd6c1ca76b89232e230ec156
Florent NOEL <florent.noel@protonmail.com>
2020-08-19 14:35:01 +0200

    Add client admin implementation

---
+++ client_admin/README.md
@@ -0,0 +1,7 @@
+### Base react project
+
+Ready to go react app basic configuration
+
+- webpack setup for dev and prod
+- babel configuration for transpiling
+- prettier and eslint for linting
---
+++ client_admin/webpack.common.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+  new webpack.HotModuleReplacementPlugin(),
+  new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Base react',
+      template: './src/index.html',
+    }),
+],
+};
