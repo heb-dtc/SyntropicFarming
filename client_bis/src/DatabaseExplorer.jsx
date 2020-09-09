7985c2d801c51ea3710c36f005698b8b55f211a2
Florent NOEL <florent.noel@protonmail.com>
2020-09-09 11:50:00 +0200

    Load filter association on DatabaseExplorer load

--- client_bis/src/DatabaseExplorer.jsx
+++ client_bis/src/DatabaseExplorer.jsx
@@ -1,56 +1,69 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import styles from './style.css'
 
-const DatabaseExplorer = ({hardiness}) => (
-  <div>
-    <ul className={styles.explorerNavigationBar}>
-      <li className={styles.navigationBarTitle}>Hardiness zone</li>
-      <li>{hardiness}</li>
-    </ul>
-    <div className={styles.explorerContainer}>
-      <aside className={styles.explorerLeftMenu}>
-        <div>SPECIES</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-      </aside>
+const baseUrl = 'https://syntropic-api.hebus.net/api'
 
-      <main>
-        <div> abaca </div>
-        <div className={styles.materialGrid}>
-          <div className={styles.gridItem}>
-            <img src="https://syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
-          </div>
-          <div className={styles.gridItem}>
-            <img src="https://Syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
-          </div>
-          <div className={styles.gridItem}>
-            <img src="https://syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
-          </div>
-          <div className={styles.gridItem}>
-            <img src="https://syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
-          </div>
-        </div>
-      </main>
+const filterSpecies = async hardiness => {
+  const response = await axios(`${baseUrl}/species`)
+  const species = response.data.filter(
+    species => species.min_hardiness >= hardiness && species.max_hardiness <= hardiness
+  )
+  return species
+}
 
-      <aside className={styles.explorerRightMenu}>
-        <div>MATERIALS</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-        <div className={styles.explorerMenuText}>amaranth</div>
-      </aside>
+const DatabaseExplorer = ({ hardiness }) => {
+  const [associations, setAssociations] = useState([])
+
+  useEffect(() => {
+    const fetchValues = async () => {
+      const response = await axios(`${baseUrl}/associations/filter/${hardiness}`)
+      setAssociations(response.data)
+    }
+    fetchValues()
+  }, [hardiness])
+
+  return (
+    <div className={styles.explorer}>
+      <ul className={styles.explorerNavigationBar}>
+        <li className={styles.navigationBarTitle}>Hardiness zone</li>
+        <li>{hardiness}</li>
+      </ul>
+      <div className={styles.explorerContainer}>
+        <aside className={styles.explorerLeftMenu}>
+          <div>SPECIES</div>
+          {associations.map(association => (
+            <div className={styles.explorerMenuText}>{association.species_name}</div>
+          ))}
+        </aside>
+
+        <main>
+          <div> abaca </div>
+          <div className={styles.materialGrid}>
+            <div className={styles.gridItem}>
+              <img src="https://syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
+            </div>
+            <div className={styles.gridItem}>
+              <img src="https://Syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
+            </div>
+            <div className={styles.gridItem}>
+              <img src="https://syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
+            </div>
+            <div className={styles.gridItem}>
+              <img src="https://syntropic-api.hebus.net/uploads/Abaca-paper.jpg" />
+            </div>
+          </div>
+        </main>
+
+        <aside className={styles.explorerRightMenu}>
+          <div>MATERIALS</div>
+          {associations.map(association => (
+            <div className={styles.explorerMenuText}>{association.material_name}</div>
+          ))}
+        </aside>
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default DatabaseExplorer
