22007c6fa258f32653c1a88dd7369f98b74f843c
Florent NOEL <florent.noel@protonmail.com>
2020-09-09 11:49:17 +0200

    Improve global app layout

--- client_bis/src/App.js
+++ client_bis/src/App.js
@@ -3,24 +3,27 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavigationBar from '@/NavigationBar'
 import LandingPage from '@/LandingPage'
 import Library from '@/Library'
+import styles from './style.css'
 
 const App = () => (
   <Router>
-    <NavigationBar />
-    <Switch>
-      <Route path="/database">
-        <LandingPage />
-      </Route>
-      <Route path="/polycultures">
-        <div>polyculture</div>
-      </Route>
-      <Route path="/program">
-        <div>program</div>
-      </Route>
-      <Route path="/">
-        <Library />
-      </Route>
-    </Switch>
+    <div className={styles.page}>
+      <NavigationBar />
+      <Switch>
+        <Route path="/database">
+          <LandingPage />
+        </Route>
+        <Route path="/polycultures">
+          <div>polyculture</div>
+        </Route>
+        <Route path="/program">
+          <div>program</div>
+        </Route>
+        <Route path="/">
+          <Library />
+        </Route>
+      </Switch>
+    </div>
   </Router>
 )
 
