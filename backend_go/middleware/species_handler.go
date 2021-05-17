47c249bdd41ecf3bb238583a6e9d1d82f42392fc
Florent NOEL <florent.noel@protonmail.com>
2021-05-17 23:21:44 +0200

    Implement edit species

--- backend_go/middleware/species_handler.go
+++ backend_go/middleware/species_handler.go
@@ -13,7 +13,43 @@ import (
 
 func EditSpecies(w http.ResponseWriter, r *http.Request) {
 	log.Printf("EditSpecies")
-	json.NewEncoder(w).Encode("{}")
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	var species models.Species
+	err := json.NewDecoder(r.Body).Decode(&species)
+
+	if err != nil {
+		log.Fatalf("Unable to decode the request body.  %v", err)
+	}
+
+	species = editSpecies(species)
+	res := response{
+		ID: species.ID,
+		Message: "Species edit successfully",
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func editSpecies(species models.Species) models.Species {
+	db := createConnection()
+	defer db.Close()
+
+	sqlStatement := `UPDATE species set name=$1, min_hardiness=$2, max_hardiness=$3 WHERE uid=$4`
+
+  //TODO: read the number of row affected by the update
+	_, err := db.Exec(sqlStatement, species.CommonName, species.MinHardiness, species.MaxHardiness, species.ID)
+
+	if err != nil {
+		log.Fatalf("Unable to execute the query, %v", err)
+	}
+
+	fmt.Printf("Edited specices %v", species.ID)
+	return species
 }
 
 func AddSpecies(w http.ResponseWriter, r *http.Request) {
@@ -40,46 +76,6 @@ func AddSpecies(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(res)
 }
 
-func GetAllSpecies(w http.ResponseWriter, r *http.Request) {
-	log.Printf("GetAllSpecies")
-
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-
-	users, err := getAllSpecies()
-
-	if err != nil {
-		log.Fatalf("Unable to get all species. %v", err)
-	}
-
-	json.NewEncoder(w).Encode(users)
-}
-
-func DeleteSpecies(w http.ResponseWriter, r *http.Request) {
-	log.Printf("DeleteSpecies")
-
-	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-	params := mux.Vars(r)
-	id, err := strconv.Atoi(params["id"])
-	if err != nil {
-		log.Fatalf("Unable to convert the string into int.  %v", err)
-	}
-
-	sqlStatement := `DELETE FROM species WHERE uid=$1`
-	deletedRows := delete(sqlStatement, int64(id))
-	msg := fmt.Sprintf("Species deleted successfully. Total rows/record affected %v", deletedRows)
-
-	res := response{
-		ID:      int64(id),
-		Message: msg,
-	}
-	json.NewEncoder(w).Encode(res)
-}
-
 func insertSpecies(species models.Species) int64 {
 	db := createConnection()
 	defer db.Close()
@@ -95,6 +91,21 @@ func insertSpecies(species models.Species) int64 {
 
 	fmt.Printf("Inserted species %v", id)
 	return id
+}
+
+func GetAllSpecies(w http.ResponseWriter, r *http.Request) {
+	log.Printf("GetAllSpecies")
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	users, err := getAllSpecies()
+
+	if err != nil {
+		log.Fatalf("Unable to get all species. %v", err)
+	}
+
+	json.NewEncoder(w).Encode(users)
 }
 
 func getAllSpecies() ([]models.Species, error) {
@@ -125,3 +136,29 @@ func getAllSpecies() ([]models.Species, error) {
 
 	return speciesList, err
 }
+
+func DeleteSpecies(w http.ResponseWriter, r *http.Request) {
+	log.Printf("DeleteSpecies")
+
+	w.Header().Set("Context-Type", "application/x-www-form-urlencoded")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int.  %v", err)
+	}
+
+	sqlStatement := `DELETE FROM species WHERE uid=$1`
+	deletedRows := delete(sqlStatement, int64(id))
+	msg := fmt.Sprintf("Species deleted successfully. Total rows/record affected %v", deletedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+	json.NewEncoder(w).Encode(res)
+}
+
